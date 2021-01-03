18d3459213902f3c267fb55669910be70f35260e
sam <wax654@gmail.com>
2021-01-03 15:16:31 -0800

    Added the last question to the js file.

--- rest_and_spread.js
+++ rest_and_spread.js
@@ -45,3 +45,52 @@ const mergeObjects = (obj1, obj2) => ({...obj1,...obj2});
      return [...arrOfNums, ...restOfNums.map((num) => num*2)];
  }
 
+/**For this section, write the following functions
+ *  using rest, spread and refactor these functions
+ *  to be arrow functions!
+ * 
+ * Make sure that you are always returning a new array 
+ *  or object and not modifying the existing inputs.
+ * 
+ * 
+//remove a random element in the items array
+//and return a new array without that item. 
+
+function removeRandom(items) {
+
+}
+
+// Return a new array with every item in array1 and array2. 
+
+function extend(array1, array2) {
+
+}
+
+// Return a new object with all the keys and values
+// from obj and a new key/value pair 
+
+function addKeyVal(obj, key, val) {
+
+}
+
+
+// Return a new object with a key removed. 
+
+function removeKey(obj, key) {
+
+}
+
+
+// Combine two objects and return a new object.
+
+function combine(obj1, obj2) {
+
+}
+
+
+// Return a new object with a modified key and value.
+
+function update(obj, key, val) {
+
+}
+ */
