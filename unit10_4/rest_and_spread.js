d91e6cd6e8765ca1bf61b7914af851f1cab2dd1e
sam <wax654@gmail.com>
2021-01-03 15:53:27 -0800

    Wrote function Combine

--- unit10_4/rest_and_spread.js
+++ unit10_4/rest_and_spread.js
@@ -134,13 +134,11 @@ const removeKey = (obj, key) =>{
 
 // Combine two objects and return a new object.
 
-function combine(obj1, obj2) {
-
-}
+const combine = (obj1, obj2) =>({...obj1,...obj2});
 
 
 // Return a new object with a modified key and value.
 
-function update(obj, key, val) {
+const update = (obj, key, val) => {
 
 }
