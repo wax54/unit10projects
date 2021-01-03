1ddb75c763488a69088223015e3c83cdab0cf571
sam <wax654@gmail.com>
2021-01-03 15:11:19 -0800

    Created mergeObjects function

--- rest_and_spread.js
+++ rest_and_spread.js
@@ -26,12 +26,22 @@ const findMin = (...nums) => {
 }
 
 /**
+ * Write a function called mergeObjects that accepts two objects
+ *  and returns a new object which contains all the keys and 
+ *  values of the first object and second object.
+ */
+
+
+const mergeObjects = (obj1, obj2) => ({...obj1,...obj2});
+
+/**
  * Write a function called doubleAndReturnArgs which accepts an 
- * array and a variable number of arguments. The function should 
- * return a new array with the original array values and all of 
- * additional arguments doubled.
+ *  array and a variable number of arguments. The function should 
+ *  return a new array with the original array values and all of 
+ *  additional arguments doubled.
  */
 
  const doubleAndReturnArgs = (arrOfNums, ...restOfNums) => {
      return [...arrOfNums, ...restOfNums.map((num) => num*2)];
  }
+
