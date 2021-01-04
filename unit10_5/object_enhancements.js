e046c270a7fe2fdee11c5dcec01ea9d0809b274b
sam <wax654@gmail.com>
2021-01-03 22:28:09 -0800

    finished objectMethods and computed properties

--- unit10_5/object_enhancements.js
+++ unit10_5/object_enhancements.js
@@ -1,5 +1,8 @@
 /**
  * Rewrite the fallowing function to ES2015 standards
+ * 
+Same keys and values
+
 ES5 Version:
 function createInstructor(firstName, lastName) {
     return {
@@ -16,3 +19,48 @@ function createInstructor(firstName,lastName){
         lastName
     };
 }
+
+
+/**
+ * Computed Property Names
+ * 
+ * ES5 Version:
+ * 
+    var favoriteNumber = 42;
+    var instructor = {
+        firstName: "Colt"
+    }
+    instructor[favoriteNumber] = "That is my favorite!"
+
+ * ES2015 Version:
+ */
+const favoriteNumber = 42;
+const instuctor = {
+    firstName: "Colt",
+    [favoriteNumber]: "That is myfavorite!"
+};
+
+/**
+ * Object Methods
+ * 
+ * ES5 Version:
+ * 
+    var instructor = {
+        firstName: "Colt",
+        sayHi: function(){
+            return "Hi!";
+        },
+        sayBye: function(){
+            return this.firstName + " says bye!";
+        }
+    }
+ * ES2015 Version:
+ */
+
+ const anotherInstructor = {
+     firstName: "Colt",
+     sayHi(){ return "Hi!";},
+     sayBye(){ return `${this.firstName} says bye!`; }
+ };
+
+ 
