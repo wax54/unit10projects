fdb5d200886ec7b7fb77847add425966495958dc
sam <wax654@gmail.com>
2021-01-03 22:33:29 -0800

    Created createAnimal Function.

--- unit10_5/object_enhancements.js
+++ unit10_5/object_enhancements.js
@@ -63,4 +63,31 @@ const instuctor = {
      sayBye(){ return `${this.firstName} says bye!`; }
  };
 
- 
+ /**
+createAnimal function
+
+Write a function which generates an animal object. The function should accepts 3 arguments:
+
+species: the species of animal (‘cat’, ‘dog’)
+
+verb: a string used to name a function (‘bark’, ‘bleet’)
+
+noise: a string to be printed when above function is called (‘woof’, ‘baaa’)
+Use one or more of the object enhancements we’ve covered.
+
+const d = createAnimal("dog", "bark", "Woooof!")
+// {species: "dog", bark: ƒ}
+d.bark()  //"Woooof!"
+
+const s = createAnimal("sheep", "bleet", "BAAAAaaaa")
+// {species: "sheep", bleet: ƒ}
+s.bleet() //"BAAAAaaaa"
+  */
+
+
+const createAnimal = (species, call, sound) => {
+    return {
+        species,
+        [call](){ return sound }
+    }
+}
